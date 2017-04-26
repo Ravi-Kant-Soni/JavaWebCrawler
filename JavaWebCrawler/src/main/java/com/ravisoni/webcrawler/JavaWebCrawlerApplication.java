608e49c16d995400dddc6e99a7c21ecb83852385
ravik_000 <ravik_000@192.168.1.5>
2017-04-27 01:42:40 +0530

    Java Web Crawler

---
+++ JavaWebCrawler/src/main/java/com/ravisoni/webcrawler/JavaWebCrawlerApplication.java
@@ -0,0 +1,12 @@
+package com.ravisoni.webcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebCrawlerApplication.class, args);
+	}
+}
